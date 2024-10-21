5707e5891f571818fbfda13e3dd48e95c18229fe
Sahil Tyagi <sahiltyagi1999@gmail.com>
2024-10-21 13:09:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+ const [color,Setcolor]= useState("olive")
+  
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1 className=' p4  text-3xl bg-slate-500 text-zinc-50' >Background Changer</h1>
+     <div className='w-full h-screen duration-200'
+     style ={{backgroundColor:color}}>
+      
+      <div className='fixed flex flex-wrap justify-center buttom-12 inset-x-0 px-2'>
+        
+        <div className="flex flex-wrap justify-center gap-10 shadow-lg bg-white px-3 py-2 rounded-3xl">
+        
+        <button className='outline-none px-4 bg-red-600 text-white shadow-lg ' onClick={()=> Setcolor("Red")}> Red Color </button>
+        <button className='outline-none px-4 bg-white text-black shadow-lg ' onClick={()=>Setcolor("White")}> White Color </button>
+        <button className='outline-none px-4 bg-black text-white shadow-lg ' onClick={()=>Setcolor("Black")}> Black Color </button>
+        <button className='outline-none px-4 bg-yellow-300 text-black shadow-lg ' onClick={()=>Setcolor("Yellow")}> Yellow Color </button>
+   
+
+        </div>
+      </div>
+     </div>
     </div>
   );
 }
